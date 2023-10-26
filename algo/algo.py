f6cb6ac550633646d59791484ca7583e0ec507c8
HarshalNallapareddy <harshalreddyn@gmail.com>
2023-10-26 15:05:33 -0400

    Added in live stock price feature

--- algo/algo.py
+++ algo/algo.py
@@ -1,54 +1,26 @@
-import requests, format_date
+import requests, format_date, yfinance as yf
 
+#compare prices of ticker from yfinance and MarketData
+## !--- NEED TO GET TOKEN FOR TICKERS OTHER THAN AAPL ---! ##
 
+tickers = ['AAPL'] 
+
+for ticker in tickers:
+    # Get price and ticker from MarketData
+    live_price_url = 'https://api.marketdata.app/v1/stocks/quotes/' + ticker
+    # mdQuote = requests.get(live_price_url, headers = {'Authorization' : 'Bearer ' + open('mdToken.txt', 'r').read()}).json()
+    # print(f'\nTicker: {mdQuote["symbol"][0]}')
+    # mdPrice = mdQuote["last"][0]
+
+    response = requests.request("GET", live_price_url)
+    mdPrice = response.json()['mid'][0]
+    print(f'{ticker} MarketData Price for: ${mdPrice:.2f}')
+
+#Get option contract data
 url = "https://api.marketdata.app/v1/options/quotes/AAPL250117C00150000/"
 
 response = requests.request("GET", url)
 data = response.json()
-
-
-
-# -- Assignment -- #
-# 1. Get the Ticker and current price of Ticker. URL: https://api.marketdata.app/v1/stocks/quotes/AAPL/
-#   - Compare price from JSON and stock quotes API to real time price. May need to get live price from other API (alphavantage)
-# 2. Get the Strike. Rule is to divide by 1000
-# 3. Get the Expiration Date in the format: January 01, 2000
-
-
-# 4. Get the Option Type (Call or Put)
-# 5. Get Delta, Gamma, and Theta
-# 6. Implement check for response code 200
-
-
-# Code for #1
-import requests, yfinance               # yfinance is not a default library
-
-tickers = ['AAPL', 'AMZN', 'MSFT', 'GOOGL', 'META', 'TSLA']
-
-for ticker in tickers:
-    # Get price and ticker from MarketData
-    url = 'https://api.marketdata.app/v1/stocks/quotes/' + ticker
-    mdQuote = requests.get(url, headers = {'Authorization' : 'Bearer ' + open('mdToken.txt', 'r').read()}).json()
-    print(f'\nTicker: {mdQuote["symbol"][0]}')
-    mdPrice = mdQuote["last"][0]
-    print(f'MarketData Price: ${mdPrice:.2f}')
-
-    # Get price from yfinance
-    aapl = yfinance.Ticker(ticker)
-    yfPrice = aapl.info['currentPrice']
-    print(f'yfinance Price: ${yfPrice:.2f}')
-
-    # Compare prices
-    print(f'Prices differ by {mdPrice - yfPrice:.2f}')
-
-
-# Code for #4, #5, #6
-
-import requests  
-
-url = "https://api.marketdata.app/v1/options/quotes/AAPL250117C00150000/"
-
-response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
